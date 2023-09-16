9f5f1dba8a2f281b9882355be2d4cca2857feb7b
João Paulo Bonagurio Ramirez <22.01247-8@maua.br>
2023-09-16 12:23:57 -0300

    BallSpawner

--- BrickBreaker/Assets/Scripts/Shooter Scripts/BallSpawner.cs
+++ BrickBreaker/Assets/Scripts/Shooter Scripts/BallSpawner.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class BallSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject playerBall;
+    [SerializeField] private InputFilter inputFilter;
+    [SerializeField] private float absoluteVelocity = 10f;
+    private ConvertVelocityVector _convertVelocityVector;
+
+    public void InstanciarBolinha(Vector2 direcao)
     {
-        
+        GameObject playerBallclone = Instantiate(playerBall, this.gameObject.transform.position, Quaternion.identity);
+        Rigidbody2D rb = playerBallclone.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = _convertVelocityVector.ConvertDirection(direcao, absoluteVelocity);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _convertVelocityVector = this.gameObject.AddComponent<ConvertVelocityVector>();
     }
+    
+    private void OnEnable()
+    {
+        inputFilter.FilteredInputEvent.AddListener(InstanciarBolinha);
+    }
+
+    private void OnDisable()
+    {
+        inputFilter.FilteredInputEvent.RemoveListener(InstanciarBolinha);
+    }
+    
+    
 }
--- BrickBreaker/Assets/Scripts/Shooter Scripts/ConversorVelocity.cs
+++ BrickBreaker/Assets/Scripts/Shooter Scripts/ConversorVelocity.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public partial class ConvertVelocityVector : MonoBehaviour
+public partial class ConversorVelocity : MonoBehaviour
 {
     public Vector2 ConvertDirection(Vector2 direction, float absoluteVelocity)
     {   
