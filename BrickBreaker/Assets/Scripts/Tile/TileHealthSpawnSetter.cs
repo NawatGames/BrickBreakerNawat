b4432d40878824d4e8603aad3818b29b3192e08a
BernardoGhioldi <bernardo.ghioldi@gmail.com>
2023-10-02 20:54:57 -0300

    Level 1
    Level 1 feito

--- BrickBreaker/Assets/Scripts/Tile/TileHealthSpawnSetter.cs
+++ BrickBreaker/Assets/Scripts/Tile/TileHealthSpawnSetter.cs
@@ -9,6 +9,10 @@ public class TileHealthSpawnSetter : MonoBehaviour
     private GameManager _gameManager;
     [SerializeField] private TileHealth tileHealth;
 
+    [SerializeField] private float steepness = 0.1f; // Ajuste a inclinação da função logística
+    [SerializeField] private float midpoint = 40f; // Ajuste a posição do ponto médio para a transição
+    [SerializeField] private float multiplier = 50f;
+
     private void Start()
     {
         _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
@@ -20,11 +24,17 @@ public class TileHealthSpawnSetter : MonoBehaviour
         int i = UnityEngine.Random.Range(1, 100);
         if (i < 20)
         {
-            tileHealth.SetHealth(_gameManager.GetMaxBallCount() * 2 + _gameManager.GetCurrentTurn());
+            tileHealth.SetHealth(_gameManager.GetMaxBallCount() * 2 + LogisticFunction(_gameManager.GetCurrentTurn()));
         }
         else
         {
-            tileHealth.SetHealth(_gameManager.GetMaxBallCount() + _gameManager.GetCurrentTurn());
+            tileHealth.SetHealth(_gameManager.GetMaxBallCount() + LogisticFunction(_gameManager.GetCurrentTurn()));
         }
+    }
+
+    private int LogisticFunction(int x)
+    {
+        // Função logística com ajuste para crescimento gradual a partir de x = 100
+        return (int)(multiplier / (1f + Mathf.Exp(-steepness * (x - midpoint))));
     }
 }
