92c49c55a91a5747376c8100be422d4f932ea088
Lelozitos <lm.fabre@hotmail.com>
2023-10-17 15:39:50 -0300

    🐛 Further Fixes

--- BrickBreaker/Assets/Scripts/Shooter Scripts/ShooterLIneRenderer.cs
+++ BrickBreaker/Assets/Scripts/Shooter Scripts/ShooterLIneRenderer.cs
@@ -34,25 +34,37 @@ public class ShooterLIneRenderer : MonoBehaviour
     
     void UpdateLineRenderer(int lines, Vector2 initialPosition, Vector2 angleClamp){
         lines++;
-        Vector3 []points = new Vector3[lines];
+        List<Vector3> points = new List<Vector3>();
 
-        points[0] = initialPosition;
+        points.Add(initialPosition);
         Vector2 temp = initialPosition;
         Vector2 angle = angleClamp;
 
         for(int n = 1; n < lines; n++){
-            RaycastHit2D hit = Physics2D.Raycast(temp + angle * .01f, angle, Mathf.Infinity);
+            RaycastHit2D hit = Physics2D.Raycast(temp + angle * .01f, angle, Mathf.Infinity, LayerMask.GetMask("Default") | LayerMask.GetMask("Tile"));
 
-            points[n] = hit.point;
-            angle = Vector2.Reflect(angle.normalized, hit.normal);
-            temp = points[n];
+            if(!hit)
+            {
+                hit = Physics2D.Raycast(temp + angle * .01f, angle);
+                AddPoint();
+                break;
+            }
+
+            AddPoint();
+
+            void AddPoint()
+            {
+                points.Add(hit.point);
+                angle = Vector2.Reflect(angle.normalized, hit.normal);
+                temp = points[n];
+            }
         }
 
         // foreach(var item in points){
         //     Debug.LogError(item);
         // }
 
-        lineRenderer.positionCount = lines;
-        lineRenderer.SetPositions(points);
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPositions(points.ToArray());
     }
 }
